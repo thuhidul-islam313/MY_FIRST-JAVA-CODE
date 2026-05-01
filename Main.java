271d5bbc852cb1f73b49d049ee302b106562b143
thuhidul islam ifty <itawhidul202@gmail.com>
2026-05-01 14:41:20 +0600

    added a new java file for variables

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Tawhidul Islam Ifty");
+        System.out.println("id : C251076");
+        System.out.println("name : tawhidul islam ifty");
+        System.out.println("semester : 3rd");
+        System.out.println("section : 3CM");
+    }
+}
---
+++ c251076_variable.java
@@ -0,0 +1,12 @@
+public class c251076_variable {
+    public static void main(String[] args) {
+        String firstname="tawhidul islam";
+        String lastname="ifty";
+        String fullname=firstname + " " + lastname;
+        System.out.println("full name :" + fullname);
+
+        int id= 251076;
+        System.out.println("id"+id);
+        
+    }
+}
