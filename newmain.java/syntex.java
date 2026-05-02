ef5fc6267ad4b88d119a6fad994f9139313ef872
thuhidul islam ifty <itawhidul202@gmail.com>
2026-05-02 21:39:02 +0600

    first

--- newmain.java/syntex.java
+++ newmain.java/syntex.java
@@ -1,6 +1,13 @@
 public class syntex{
     public static void main(String[] args){
-        int age = 18;
-        System.out.println("is age >=18 ? : " + (age>=18) );
+        String country="india";
+        if(country.equals("india")){
+            System.out.println("you are randia !!!!!!!! " );
+        }else{
+            System.out.println("you are safe??????" );
+        }
+        int time=20;
+        String greeting = (time>=18) ? "good evening" : "good morning";
+        System.out.println(greeting);
     }
 }
